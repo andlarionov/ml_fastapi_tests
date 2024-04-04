83f076dca4233c34fd1c463864cadd857a51693b
andlarionov <avl2709979@gmail.com>
2024-04-04 19:59:46 +0000

    Added new API method for NER

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 
-
 class Item(BaseModel):
     text: str
 
-
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
-
+ner_classifier = pipeline("ner")
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
-
 @app.post("/predict/")
 def predict(item: Item):
     return classifier(item.text)[0]
+
+@app.post("/ner/")
+def recognize_entities(item: Item):
+    return ner_classifier(item.text)
